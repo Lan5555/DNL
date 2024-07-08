641024d9d56c421d7edacf31dc6793ee5c62278d
Lan5555 <okekejohnson24@gmail.com>
2024-07-08 17:27:59 +0100

    beta

--- beta/Next/main.js
+++ beta/Next/main.js
@@ -4,11 +4,6 @@ let numberOfCards = 20;
 let ids = 0;
 let grid;
 let card;
-let card2;
-let cardId;
-let cardText = [];
-const mediaQuery = window.matchMedia('(max-width: 600px)');
-
 
 window.onload = () => {
 
@@ -16,53 +11,9 @@ window.onload = () => {
       grid = localStorage.getItem('list');
     for(let i = 1; i <= numberOfCards; i++){
       ids++;
-       cardId =  `card${ids}`
-       
-      console.log(cardId)
-      if(grid == "6" && mediaQuery.matches){
-        const cardHtml = ` <div class = "col-${grid} col-md-6 col-lg-4 col-xl-4">
-                    <div class = "card card-custom" id = "${cardId}">
-                      <img src = "src/img/fruite-item-1.jpg" class = "card-img-top" alt="photo" id = "imageDisplay">
-                      <div class = "card-body">
-                       <h1 class = "card-title"></h1> 
-                       <p class = "card-text" ></p> 
-                     <p id = "price" >
-                     </p>
-                     <a id = "more" onclick = "details('${cardId}')">More..</a>
-                      </div>
-                    </div>
-                  </div>`;
-              cards.innerHTML += cardHtml;
-              
-              
-              
-              editCardContents('card3','','','src/img/fruite-item-2.jpg','');
-      }else{
-      const cardHtml = ` <div class = "col-${grid} col-md-6 col-lg-4 col-xl-4">
-            <div class = "card card-custom" id = "${cardId}">
-              <img src = "src/img/fruite-item-1.jpg" class = "card-img-top" alt="photo" id = "imageDisplay">
-              <div class = "card-body">
-                <h1 class = "card-title">Test</h1>
-                <p class = "card-text">This is a test</p>
-                <p id = "price">Price: NGN 200</p>
-                <button class = "btn btn-primary">Add to cart <i class = "bi bi-cart"></i></button>
-              </div>
-            </div>
-          </div>`;
-          cards.innerHTML += cardHtml;
-          editCardContents('card3','Test3','ThirdTest','src/img/fruite-item-2.jpg','4000');
-      }
-            
-    }
-      
-    }else{
-      for(let i = 1; i <= numberOfCards; i++){
-
-      ids++;
-
       const cardId =  `card${ids}`
       console.log(cardId)
-      const cardHtml = ` <div class = "col-12 col-md-6 col-lg-4 col-xl-4">
+      const cardHtml = ` <div class = "col-${grid} col-md-6 col-lg-4 col-xl-4">
             <div class = "card card-custom" id = "${cardId}">
               <img src = "src/img/fruite-item-1.jpg" class = "card-img-top" alt="photo" id = "imageDisplay">
               <div class = "card-body">
@@ -77,6 +28,8 @@ window.onload = () => {
     }
       
       editCardContents('card3','Test3','ThirdTest','src/img/fruite-item-2.jpg','4000');
+    }else{
+
     }
  
 }
@@ -91,13 +44,6 @@ const editCardContents = (cardId, newTitle, newText, newImageUrl,newPrice) => {
    card.querySelector('#price').textContent = newPrice;
  }
 };
-
-
-
-const details = (cardId) => {
-  localStorage.setItem('selectedCardId', cardId);
-  window.location.href = "details.html";
-}
 
 function cart(){
   window.location.href = "cart.html";
